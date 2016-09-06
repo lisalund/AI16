d8251b61e173ddc4a02f63f2bfe9ea4a854b7dd3
augustjanse <augustj@kth.se>
2016-09-06 12:21:19 +0200

    Implement multiply with small (insufficient) test

--- HW1/src/MatrixOperator.java
+++ HW1/src/MatrixOperator.java
@@ -1,11 +1,22 @@
 public class MatrixOperator {
 	public static void main(String[] args) {
-		double[][] res = multiply(new double[5][3], new double[3][6]);
-		printMatrix(res);
+		runTests();
 	}
 
 	public static double[][] multiply(double[][] a, double[][] b) {
 		double[][] res = new double[a.length][b[0].length];
+
+		int rows = res.length;
+		int cols = res[0].length;
+		int innerLength = a[0].length; // == b.length, m in formula
+
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				for (int k = 0; k < innerLength; k++) {
+					res[i][j] += a[i][k] * b[k][j];
+				}
+			}
+		}
 
 		return res;
 	}
@@ -17,5 +28,21 @@ public class MatrixOperator {
 			}
 			System.out.println();
 		}
+	}
+
+	public static void runTests() {
+		double[][] a = {
+				{1, 2, 3},
+				{4, 5, 6}
+		};
+
+		double[][] b = {
+				{7, 8},
+				{9, 10},
+				{11, 12}
+		};
+
+		double[][] res = multiply(a, b);
+		printMatrix(res);
 	}
 }
