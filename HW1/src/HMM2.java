1ebf13a3f01353a5a2871d784f08cb00ca3f5460
lisalund <lisa.lund93@gmail.com>
2016-09-12 10:19:35 +0200

    added skeleton for HMM2

---
+++ HW1/src/HMM2.java
@@ -0,0 +1,23 @@
+
+public class HMM2 {
+	
+	/**
+	 * Matrices A (transitions), B (emissions), and pi (initial distributions)
+	 */
+	private Matrix A;
+	private Matrix B;
+	private Matrix pi;
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new HMM2();
+
+	}
+	
+	HMM2(){
+		
+	}
+
+}
