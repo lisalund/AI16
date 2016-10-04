f375e096f395f3e5da8f366368a5aaab70c14b90
augustjanse <augustj@kth.se>
2016-10-04 15:05:50 +0200

    Fix bug when calculating secondMaxCol

--- HW1/src/HMM3.java
+++ HW1/src/HMM3.java
@@ -22,7 +22,7 @@ public class HMM3 {
 			// For each "max column" (as many as the number of states)
 			for (int j = 0; j < a.columns; j++) {
 				Matrix firstMaxCol = new Matrix(a.rows, 1, prevDeltaCol.getElement(j, 0));
-				Matrix secondMaxCol = a.getRow(o[i]).transpose();
+				Matrix secondMaxCol = a.getRow(j).transpose();
 				Matrix thirdMaxCol = b.getColumn(o[i]);
 
 				Matrix fullMaxCol = firstMaxCol.multiplyElementWise(secondMaxCol).multiplyElementWise(thirdMaxCol);
