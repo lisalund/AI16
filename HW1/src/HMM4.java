531dbbd44dfbb3c08c30ca8ce4c3890d9708d9ac
augustjanse <augustj@kth.se>
2016-11-08 18:27:55 +0100

    Use m, n and big_t

--- HW1/src/HMM4.java
+++ HW1/src/HMM4.java
@@ -12,6 +12,10 @@ public class HMM4 {
 	private double[][][] digamma;
 	private double[] scale;
 
+	private int n;
+	private int m;
+	private int big_t;
+
 	private HMM4() {
 		// Parse input
 		Utility u = new Utility();
@@ -20,6 +24,10 @@ public class HMM4 {
 		this.b = u.parseMatrix();
 		this.pi = u.parseMatrix();
 		this.o = u.parseEmissions();
+
+		n = b.rows;
+		m = b.columns;
+		big_t = o.length;
 
 		int i = 0; // iters
 		double oldLogProb;
@@ -51,23 +59,23 @@ public class HMM4 {
 
 		// Compute alpha_0(i) using pi
 		c[0] = 0;
-		for (int i = 0; i < a.rows; i++) {
+		for (int i = 0; i < n; i++) {
 			alpha[0][i] = this.pi.mat[0][i] * this.b.mat[i][this.o[0]];
 			c[0] = c[0] + alpha[0][i];
 		}
 
 		// Scale alpha_0(i)
 		c[0] = 1 / c[0];
-		for (int i = 0; i < a.rows; i++) {
+		for (int i = 0; i < n; i++) {
 			alpha[0][i] = c[0] * alpha[0][i];
 		}
 
 		// Compute alpha_t(i)
-		for (int t = 1; t < this.o.length; t++) {
+		for (int t = 1; t < big_t; t++) {
 			c[t] = 0;
-			for (int i = 0; i < this.a.rows; i++) {
+			for (int i = 0; i < n; i++) {
 				alpha[t][i] = 0;
-				for (int j = 0; j < this.a.rows; j++) {
+				for (int j = 0; j < n; j++) {
 					alpha[t][i] = alpha[t][i] + alpha[t - 1][j] * this.a.mat[j][i];
 				}
 				alpha[t][i] = alpha[t][i] * this.b.mat[i][this.o[t]];
@@ -76,7 +84,7 @@ public class HMM4 {
 
 			// Scale alpha_t(i)
 			c[t] = 1 / c[t];
-			for (int i = 0; i < this.a.rows; i++) {
+			for (int i = 0; i < n; i++) {
 				alpha[t][i] = c[t] * alpha[t][i];
 			}
 		}
@@ -89,16 +97,16 @@ public class HMM4 {
 		double[][] beta = new double[this.o.length][this.a.rows];
 
 		// Set beta_T-1(i) to 1 scaled by c_T-1
-		for (int i = 0; i < this.a.rows; i++) {
-			beta[this.o.length - 1][i] = this.scale[this.o.length - 1];
+		for (int i = 0; i < n; i++) {
+			beta[big_t - 1][i] = this.scale[big_t - 1];
 		}
 
 		// Let the consecutive beta pass begin
-		for (int t = this.o.length - 2; t >= 0; t--) {
-			for (int i = 0; i < this.a.rows; i++) {
+		for (int t = big_t - 2; t >= 0; t--) {
+			for (int i = 0; i < n; i++) {
 				beta[t][i] = 0;
 
-				for (int j = 0; j < this.a.rows; j++) {
+				for (int j = 0; j < n; j++) {
 					beta[t][i] = beta[t][i] + this.a.mat[i][j]
 							* this.b.mat[j][this.o[t + 1]]
 							* beta[t + 1][j];
@@ -114,13 +122,13 @@ public class HMM4 {
 
 	private void gammas() {
 		double denom;
-		double[][] gamma = new double[this.o.length][this.a.rows];
-		double[][][] digamma = new double[this.o.length][this.a.rows][this.a.rows];
+		double[][] gamma = new double[big_t][n];
+		double[][][] digamma = new double[big_t][n][n];
 
-		for (int t = 0; t <= this.o.length - 2; t++) {
+		for (int t = 0; t <= big_t - 2; t++) {
 			denom = 0;
-			for (int i = 0; i < this.a.rows; i++) {
-				for (int j = 0; j < this.a.rows; j++) {
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
 					denom = denom + (alphaProb[t][i]
 							* this.a.mat[i][j]
 							* this.b.mat[j][this.o[t + 1]]
@@ -128,10 +136,10 @@ public class HMM4 {
 				}
 			}
 
-			for (int i = 0; i < this.a.rows; i++) {
+			for (int i = 0; i < n; i++) {
 				gamma[t][i] = 0;
 
-				for (int j = 0; j < a.rows; j++) {
+				for (int j = 0; j < n; j++) {
 					digamma[t][i][j] = (this.alphaProb[t][i]
 							* this.a.mat[i][j]
 							* this.b.mat[j][this.o[t + 1]]
@@ -143,12 +151,12 @@ public class HMM4 {
 
 		// Handle last gamma
 		denom = 0;
-		for (int i = 0; i < this.a.rows; i++) {
-			denom = denom + this.alphaProb[this.o.length - 1][i];
+		for (int i = 0; i < n; i++) {
+			denom = denom + this.alphaProb[big_t - 1][i];
 		}
 
-		for (int i = 0; i < this.a.rows; i++) {
-			gamma[this.o.length - 1][i] = this.alphaProb[this.o.length - 1][i] / denom;
+		for (int i = 0; i < n; i++) {
+			gamma[big_t - 1][i] = this.alphaProb[big_t - 1][i] / denom;
 		}
 
 		this.gamma = gamma;
@@ -162,15 +170,15 @@ public class HMM4 {
 		double[][] pi = new double[this.pi.rows][this.pi.columns];
 
 		// Re-estimate pi
-		System.arraycopy(this.gamma[0], 0, pi[0], 0, this.a.rows);
+		System.arraycopy(this.gamma[0], 0, pi[0], 0, n);
 
 		// Re-estimate a
-		for (int i = 0; i < this.a.rows; i++) {
-			for (int j = 0; j < this.a.columns; j++) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
 				numer = 0;
 				denom = 0;
 
-				for (int t = 0; t < this.o.length - 1; t++) {
+				for (int t = 0; t <= big_t - 2; t++) {
 					numer = numer + this.digamma[t][i][j];
 					denom = denom + this.gamma[t][i];
 				}
@@ -180,12 +188,12 @@ public class HMM4 {
 		}
 
 		// Re-estimate b
-		for (int i = 0; i < this.b.rows; i++) {
-			for (int j = 0; j < this.b.columns; j++) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
 				numer = 0;
 				denom = 0;
 
-				for (int t = 0; t < this.o.length - 1; t++) {
+				for (int t = 0; t < big_t - 1; t++) {
 					if (this.o[t] == j) {
 						numer = numer + this.gamma[t][i];
 					}
@@ -199,15 +207,13 @@ public class HMM4 {
 		this.pi = new Matrix(pi);
 		this.a = new Matrix(a);
 		this.b = new Matrix(b);
-
 	}
 
 	private void log() {
 		double logProb = 0;
-		for (int t = 0; t < this.o.length; t++) {
+		for (int t = 0; t < big_t; t++) {
 			logProb = logProb + Math.log(this.scale[t]);
 		}
 		this.logProb = -1 * logProb;
 	}
-
 }
